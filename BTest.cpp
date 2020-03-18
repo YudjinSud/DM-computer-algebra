9ae3db4f9bedc1f2f1d3fc01242267bfa495c9d0
elek34ka <xtkcbfgkqwerty@gmail.com>
2020-03-18 12:49:20 +0300

    N_7.1_with_all_tests

--- BTest.cpp
+++ BTest.cpp
@@ -4,7 +4,10 @@ void BTest::runAllTests() {
     test_COM_NN_D();
     test_NZER_N_B();
     test_ADD_1N_N();
-
+    test_ADD_NN_N();
+    test_SUB_NN_N();
+    test_MUL_ND_N();
+    test_MUL_Nk_N();
 }
 
 void BTest::test_COM_NN_D() {
@@ -58,4 +61,52 @@ void BTest::test_ADD_1N_N() {
     res.dig = {1,0,0,0};
     DO_CHECK(ADD_1N_N(a) == res);
 
+}
+
+void BTest::test_ADD_NN_N() {
+    Natural a,b;
+
+    a.n = 3;
+    a.dig = {9,9,9};
+    b.n = 3;
+    b.dig = {9,9,9};
+    Natural res;
+    res.n = 4;
+    res.dig = {1,9,9,8};
+    DO_CHECK(ADD_NN_N(a,b) == res);
+}
+
+void BTest::test_SUB_NN_N() {
+    Natural a,b;
+
+    a.n = 2;
+    a.dig = {9,9};
+    b.n = 3;
+    b.dig = {9,9,9};
+    Natural res;
+    res.n = 3;
+    res.dig = {9,0,0};
+    DO_CHECK(SUB_NN_N(a,b) == res);
+}
+
+void BTest::test_MUL_ND_N() {
+    Natural a;
+    int n = 8;
+    a.n = 2;
+    a.dig = {9,9};
+    Natural res;
+    res.n = 3;
+    res.dig = {7,9,2};
+    DO_CHECK(MUL_ND_N(a,n) == res);
+}
+
+void BTest::test_MUL_Nk_N() {
+    Natural a;
+    int k = 4;
+    a.n = 2;
+    a.dig = {9,9};
+    Natural res;
+    res.n = 6;
+    res.dig = {9,9,0,0,0,0};
+    DO_CHECK(MUL_Nk_N(a,k) == res);
 }
